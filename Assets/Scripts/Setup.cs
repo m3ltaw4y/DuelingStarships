57298478d177e3037d12d45b60b426b99a8e3136
M3LTAW4Y <m3ltaw4y@gmail.com>
2021-03-07 12:13:39 -0800

    initial code commit.  stars, movement working

---
+++ Assets/Scripts/Setup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Setup : MonoBehaviour
+{
+    [SerializeField] private GameObject star;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (var i = 0; i < 100; i++)
+        {
+            var newStar = Instantiate(star, new Vector3(Random.Range(-640, 641), Random.Range(-360, 361), 0), Quaternion.identity);
+            newStar.GetComponent<SpriteRenderer>().color = new Color(Random.Range(.5f,1), 1, 1);
+        }
+    }
+
+    
+    void Update()
+    {
+    }
+}
+
+//Screen.SetResolution(1280, 720, true);
+//Camera.main.aspect = 1280 / 720f;
+//Camera.main.pixelRect = new Rect(0,0,1280,720);
+//GetComponent<Camera>().pixelRect = new Rect(0,0,1280,720);
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Ship : MonoBehaviour
+{
+     [SerializeField] private int playerIndex;
+     [SerializeField] private float angle;
+     private Vector2 inputVec;
+     private float accel;
+     private void Awake()
+     {
+          transform.position = new Vector2(playerIndex == 0 ? -400 : 400, UnityEngine.Random.Range(-250, 250));
+     }
+
+     private void FixedUpdate()
+     {
+          transform.position = new Vector2((Math.Abs(transform.position.x + 1240 + 640)) % 1240 - 640, Math.Abs((transform.position.y + 720 + 360)) % 720 - 360);//screen wrap
+          angle += inputVec.x * -3;
+          transform.rotation = Quaternion.Euler(new Vector3(0,0,angle));
+          GetComponent<Rigidbody2D>().AddForce(240 * accel * transform.right);
+     }
+
+     private void Update()
+     {
+          //if (Input.GetButtonDown("Fire1"))
+          //     Instantiate(projectile, transform.position, transform.rotation);
+          //if (Input.GetButtonDown(accel))
+          //     GetComponent<Rigidbody2D>().AddForce(new Vector2(0.1f, 0.1f));
+     }
+     
+     
+     public void OnFire()
+     {
+          Debug.Log("Fire!");
+     }
+    
+     public void OnBomb()
+     {
+          Debug.Log("Bomb!");
+     }
+    
+     public void OnTurn(InputValue input)
+     {
+          inputVec = input.Get<Vector2>();
+     }
+
+     public void OnAccel(InputValue input)
+     {
+          accel = input.Get<float>();
+          Debug.Log("Accel!");
+     }
+}
