3ea729f829d6c8f04e6c9f336c11a2441b2ae849
M3LTAW4Y <m3ltaw4y@gmail.com>
2021-03-07 18:58:44 -0800

    just a rename

--- Assets/Scripts/Mine.cs
+++ Assets/Scripts/Mine.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-
 public class Mine : Bullet {
     public override int LifeTime => 10;
     public void Awake() => GetComponent<Rigidbody2D>().velocity = 2 * new Vector2(Random.Range(-1f,1f),Random.Range(-1f,1f)).normalized;
